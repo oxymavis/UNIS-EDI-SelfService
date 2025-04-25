d3a752a71882f093ceabc5fa435681ea9166372c
Shelia <shelia@example.com>
2025-04-25 22:18:20 +0800

    Update next.config.mjs: Remove instrumentationHook as it's no longer needed

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,35 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://unis-edi.ngrok.app' : '',
+  basePath: '',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
+  },
+  allowedDevOrigins: ['unis-edi.ngrok.app', '*.ngrok.app'],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['unis-edi.ngrok.app', '*.ngrok.app'],
+    },
+  },
 }
 
 export default nextConfig
